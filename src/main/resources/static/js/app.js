510ea05fe0ec399d80c5b0ac1b8d7f1c657e9abd
Marcus <mgb.klimstra@gmail.com>
2014-09-21 15:37:42 +0200

    Add ember-schema-loader dependency and load the schema.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,7 +4,21 @@ var App = Ember.Application.createWithMixins({
   LOG_VIEW_LOOKUPS: true,
   LOG_STACKTRACE_ON_DEPRECATION: true,
   LOG_VERSION: true,
-  debugMode: true,  
+  debugMode: true,
+  
+  init: function() {
+    var app = this;
+    app.deferReadiness();
+
+    $.ajax({ 
+      url: 'http://localhost:8080/ember-schema'
+    }).then(function(schema) {
+      var schemaLoader = SchemaLoader.create({ target: app, container: app });
+      schemaLoader.load(schema);
+      app.advanceReadiness();
+    });
+    this._super();
+  }
 });
 
 App.ApplicationAdapter = DS.RESTAdapter;
